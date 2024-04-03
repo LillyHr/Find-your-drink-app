6e8a6fbc1510205ea6d8f12b225c4975787c4041
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2024-04-03 23:41:02 +0300

    adjusting components

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAIN } from 'src/app/constants';
+import { emailValidator } from 'src/app/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/utils/mathc-passwords-validator';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +12,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required]],
+    email: ['', [Validators.required, emailValidator(EMAIL_DOMAIN)]],
+    tel: [''],
+    passGroup: this.fb.group({
+      password: ['', Validators.required],
+    rePassword: ['', Validators.required]
+    }, {
+      validators: [matchPasswordsValidator('password', 'rePassword')],
+    })
+    })
+    
+    constructor(
+      private fb: FormBuilder,
+      private userService: UserService,
+      private router: Router
+      ) {}
+    
+    register(): void {
+      if (this.form.invalid)  {
+        return;
+      }
 
+      const { username, email, tel, 
+        passGroup: { password, rePassword } = {}, 
+      } = this.form.value;
+
+    this.userService
+    .register(username!, email!, tel!, password!, rePassword! )
+    .subscribe(() => {
+      this.router.navigate(['/home']);
+    })
+    }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { UserAuth } from '../types/user';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
+export class UserService implements OnDestroy {
+private user$$ = new BehaviorSubject<UserAuth | undefined >(undefined);
+private user$ = this.user$$.asObservable();
+
 user: UserAuth | undefined;
 USER_KEY = '[user]';
 
-  constructor(private http: HttpClient) { 
-     try {
-    const lsUser = localStorage.getItem(this.USER_KEY) || '';
-    this.user = JSON.parse(lsUser);
-  } catch (error) {
-    this.user = undefined;
-  } 
+userSubscription: Subscription;
+  constructor(private http: HttpClient) {
+     this.userSubscription = this.user$.subscribe(user => {
+      this.user = user;
+    })
   }
 
 
   login(email: string, password: string) {
 return this.http.post<UserAuth>('/api/login', { email, password })
+.pipe(tap(user => {this.user$$.next(user);}))
   }
 
   register(
@@ -30,11 +33,17 @@ return this.http.post<UserAuth>('/api/login', { email, password })
     password: string, 
     rePassword: string,
   ) {
-    return this.http.post<UserAuth>('/api/register', { username, email, tel, password, rePassword })
+    return this.http.post<UserAuth>('/api/register', 
+    { username, email, tel, password, rePassword })
+    .pipe(tap(user => {this.user$$.next(user);}))
   }
 
   logout() {
-    localStorage.removeItem(this.USER_KEY);
-    this.user = undefined;
+    return this.http.post<UserAuth>('/api/logout', {})
+    .pipe(tap(() => {this.user$$.next(undefined)}));
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
